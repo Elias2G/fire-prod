e1ad4b02c7cbd1d4edbc3d52081f87ca80e6f36c
XImOnTRiX <domenik.wenghofer@gmail.com>
2019-02-25 15:47:01 +0100

    s

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var app = express();
 
 var pool = mysql.createPool({
   connectionLimit: 100,
-  host: 'http://157.230.106.121',
+  host: '157.230.106.121',
   user: 'remote',
   password: 'Richtig_Sicher2019',
   database: 'ff-kappel',
