d1f9621b3cf4d6ea5b48916aa53fc7cd5b06358b
XImOnTRiX <domenik.wenghofer@gmail.com>
2019-02-26 14:17:34 +0100

    finally?

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,5 +1,5 @@
 
-export const ROOT_URL = 'http://localhost:8080';
+export const ROOT_URL = 'http://157.230.106.121';
 
 export const getAll = '/api/get/all';
 export const getEinsatz = '/api/get/einsaetze';
