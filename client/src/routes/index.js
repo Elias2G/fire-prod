b82b45a21d8a7dcb4d67c0d95fdbbf928529da69
XImOnTRiX <domenik.wenghofer@gmail.com>
2019-03-07 10:23:28 +0100

    f

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -106,7 +106,7 @@ export class Navigation extends Component {
       return(
         <>
         <div className="desktop shadow row">
-          <div className="column col-md-2"><h4 className="nb">FF-Kappel</h4></div>
+          <div className="column col-md-2"><h4 className="nb regular">FF-Kappel</h4></div>
           <div className="column col-md-10 justify-center">
             <div className="row justify-end">
               <div className="column test">
